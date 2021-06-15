888f79217b9fc0409e8cd605b150b0bc5b54cea7
claudvanbarret <claudvanbarreto@hotmail.com>
2021-06-15 16:11:50 -0300

    feat: add api docs

--- server/server.js
+++ server/server.js
@@ -2,7 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const dotenv = require("dotenv");
+const yaml = require("yamljs");
+const swaggerUi = require("swagger-ui-express");
 const routes = require("./src/routes");
+
+const swaggerDocument = yaml.load("./swagger.yaml");
 
 const app = express();
 
@@ -10,7 +14,8 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use("/", routes);
+app.use("/api", routes);
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 dotenv.config();
 
