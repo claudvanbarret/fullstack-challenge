a8b03193779bcb907c183b940b258e8a65a89818
claudvanbarret <claudvanbarreto@hotmail.com>
2021-06-17 17:49:31 -0300

    feat: add route guard

--- client/src/components/Navigation/Navigation.jsx
+++ client/src/components/Navigation/Navigation.jsx
@@ -1,14 +1,36 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import { useDispatch } from "react-redux";
+import { Link, useHistory } from "react-router-dom";
 import { Menu } from "antd";
-
+import { LoginOutlined } from "@ant-design/icons";
 import routes from "../../configs/routes";
+import { logout } from "../../services/auth.service";
 import * as S from "./Navigation.styles";
+import { setUser } from "../../store/user/actions";
 
 const Navigation = () => {
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const { t } = useTranslation();
   const [current, setCurrent] = useState("professionals");
 
+  /**
+   * @description Changes the menu item
+   */
   const handleClick = (e) => setCurrent(e.key);
+
+  /**
+   * @description Log out of the application
+   */
+  const handleLogout = () => {
+    // Clear session
+    logout();
+    // Clear user store
+    dispatch(setUser(null));
+    // Redirects to login
+    history.push("/");
+  };
 
   return (
     <S.Menu onClick={handleClick} selectedKeys={[current]} mode="vertical">
@@ -17,6 +39,9 @@ const Navigation = () => {
           <Link to={to}>{label}</Link>
         </Menu.Item>
       ))}
+      <S.MenuLogout key="logout" icon={<LoginOutlined />} onClick={handleLogout}>
+        {t("general.logout")}
+      </S.MenuLogout>
     </S.Menu>
   );
 };
--- client/src/router/index.jsx
+++ client/src/router/index.jsx
@@ -1,16 +1,37 @@
-import { Switch, Route } from "react-router-dom";
-
+import { Switch, Route, Redirect } from "react-router-dom";
+import { useSelector } from "react-redux";
 import { Layout } from "../layout";
 import { Professional } from "../pages/Professional";
 import { ProfessionalType } from "../pages/ProfessionalType";
+import { Login } from "../pages/Login";
 
-const Router = () => (
-  <Switch>
-    <Layout>
-      <Route exact path="/professionals" component={Professional} />
-      <Route exact path="/professionaltypes" component={ProfessionalType} />
-    </Layout>
-  </Switch>
-);
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const { user } = useSelector((state) => state.user);
+
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        user ? <Component {...props} /> : <Redirect to={{ pathname: "/", state: { from: props.location } }} />
+      }
+    />
+  );
+};
+
+const Router = () => {
+  const { loadingUser } = useSelector((state) => state.user);
+
+  return (
+    <Switch>
+      <Route exact path="/" component={Login} />
+      {!loadingUser && (
+        <Layout>
+          <PrivateRoute exact path="/professionals" component={Professional} />
+          <PrivateRoute exact path="/professionaltypes" component={ProfessionalType} />
+        </Layout>
+      )}
+    </Switch>
+  );
+};
 
 export default Router;
